083900066d59e7917d2b81fc75fe561102488410
zytzyt1 <2675213201@qq.com>
2019-09-29 11:09:59 +0800

    Spring boot 实验示例

---
+++ java_code02/src/main/java/com/hunau/HelloController.java
@@ -0,0 +1,15 @@
+package com.hunau;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by sang on 2018/7/4.
+ */
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "学号：201841882319 周宇滔";
+    }
+}
